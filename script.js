5559b0eaec7c2ff8665af258ebb4c12e7e5d12d0
Lateefat Omotoso <omotosolateefat23@gmail.com>
2024-03-13 10:31:15 -0700

    added a new JavaScript

--- script.js
+++ script.js
@@ -3,24 +3,24 @@ let score1 = score.value;
 
 
     if (score1 > 0 && score1 < 40) {
-        document.getElementById("result").innerHTML = "fail";
+        document.getElementById("result").innerHTML = "Fail";
         
     } else if (score1 >= 39 && score1 < 45) {
-    document.getElementById("result").innerHTML = "pass";
+    document.getElementById("result").innerHTML = "Pass";
     
     }
     else if (score1 >= 45 && score1 < 50) {
-        document.getElementById("result").innerHTML = "credit";
+        document.getElementById("result").innerHTML = "Credit";
         var songFailure = new Audio('audio/BTS-Life-Goes-On-(TrendyBeatz.com).mp3')
         songFailure.play()
 
         }
         else if (score1 >= 50 && score1 < 60) {
-            document.getElementById("result").innerHTML = "good";
+            document.getElementById("result").innerHTML = "Good";
                 
             }
             else if (score1 >= 60 && score1 < 70) {
-                document.getElementById("result").innerHTML = "wonderful";
+                document.getElementById("result").innerHTML = "Wonderful";
                     
                 }
                 else if (score1 >= 70 && score1 <= 100) {
@@ -28,11 +28,11 @@ let score1 = score.value;
                         
                     }
                     else if (score1 < 0) {
-                        document.getElementById("result").innerHTML = "invalid score";
+                        document.getElementById("result").innerHTML = "Invalid score";
                         
                         }
                         else if (score1 >= 100) {
-                            document.getElementById("result").innerHTML = "Too much";
+                            document.getElementById("result").innerHTML = "Pls reduce the number";
                                 
                             }
                             else if (score1 === '') {
